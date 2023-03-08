957d03d8d11fcdd13e213d49cc4b9b24d995319d
Gakai <github@skutscher.de>
2023-03-08 23:57:32 +0000

    feat: ask username and lookup id

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
 GOAL=$(( RANDOM % 1000 + 1 ))
+
+echo "Enter your username:"
+read NAME
+if [[ -z $NAME ]]; then exit; fi
+USER_ID=$($PSQL "select user_id from users where name='$NAME'")
