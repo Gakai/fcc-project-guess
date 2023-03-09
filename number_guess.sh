ff3ad77a176ce4d68b0fa1fd14e80bdcd7c3dc32
Gakai <github@skutscher.de>
2023-03-09 01:00:39 +0000

    feat: added guessing logic

--- number_guess.sh
+++ number_guess.sh
@@ -11,7 +11,11 @@ USER_ID=$($PSQL "select user_id from users where name=trim('$NAME')")
 # if not found
 if [[ -z $USER_ID ]]; then
   # save new user
-  INSERT_USER_RESULT="$($PSQL "insert into users(name) values(trim('$NAME'))")"
+  INSERT_USER_RESULT="$($PSQL "insert into users(name) values(trim('$NAME')) returning user_id")"
+  # inserting failed?
+  if [[ -z $INSERT_USER_RESULT ]]; then exit; fi
+  # save new user id for later
+  USER_ID="$(echo "$INSERT_USER_RESULT" | head -1 )"
   # greet new user
   echo "Welcome, $NAME! It looks like this is your first time here."
 else
@@ -19,3 +23,20 @@ else
   readarray -t GAME_STATS <<< "$(echo "$GAMES_RESULT" | sed 's/|/\n/g')"
   echo "Welcome back, $NAME! You have played ${GAME_STATS[0]} games, and your best game took ${GAME_STATS[1]} guesses."
 fi
+
+echo "Guess the secret number between 1 and 1000:"
+GUESSES=1
+read GUESS
+until (( GUESS == GOAL )); do
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+  elif (( GUESS > GOAL )); then
+    echo "It's lower than that, guess again:"
+  else
+    echo "It's higher than that, guess again:"
+  fi
+  (( GUESSES++ ))
+  read GUESS
+done
+echo "You guessed it in $GUESSES tries. The secret number was $GOAL. Nice job!"
+ADD_GAME_RESULT="$($PSQL "insert into games(user_id, guesses) values($USER_ID, $GUESSES)")"
