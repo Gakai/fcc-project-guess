0b2a9471d26523a0a29271c31faf9b8f55c2cc04
Gakai <github@skutscher.de>
2023-03-09 00:28:33 +0000

    feat: greet returning player

--- number_guess.sh
+++ number_guess.sh
@@ -14,4 +14,8 @@ if [[ -z $USER_ID ]]; then
   INSERT_USER_RESULT="$($PSQL "insert into users(name) values(trim('$NAME'))")"
   # greet new user
   echo "Welcome, $NAME! It looks like this is your first time here."
+else
+  GAMES_RESULT="$($PSQL "select count(*), min(guesses) from games where user_id=$USER_ID group by user_id")"
+  readarray -t GAME_STATS <<< "$(echo "$GAMES_RESULT" | sed 's/|/\n/g')"
+  echo "Welcome back, $NAME! You have played ${GAME_STATS[0]} games, and your best game took ${GAME_STATS[1]} guesses."
 fi
