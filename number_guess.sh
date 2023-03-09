5505b478ea50098b8ba8713623e323d5f05fde59
Gakai <github@skutscher.de>
2023-03-09 00:10:44 +0000

    feat: create new user

--- number_guess.sh
+++ number_guess.sh
@@ -7,4 +7,11 @@ GOAL=$(( RANDOM % 1000 + 1 ))
 echo "Enter your username:"
 read NAME
 if [[ -z $NAME ]]; then exit; fi
-USER_ID=$($PSQL "select user_id from users where name='$NAME'")
+USER_ID=$($PSQL "select user_id from users where name=trim('$NAME')")
+# if not found
+if [[ -z $USER_ID ]]; then
+  # save new user
+  INSERT_USER_RESULT="$($PSQL "insert into users(name) values(trim('$NAME'))")"
+  # greet new user
+  echo "Welcome, $NAME! It looks like this is your first time here."
+fi
